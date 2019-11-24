50f453621d4ff33b176b32308595b00b3fadf6ca
jianghy <tzword@163.com>
2019-11-24 22:16:46 +0800

    不安全线程的演示

---
+++ src/main/java/com/tzword/concurrency/ConcurrencyTest.java
@@ -0,0 +1,44 @@
+package com.tzword.concurrency;
+
+import com.tzword.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+    //请求总数
+    public static int CLIENT_TOTAL = 5000;
+    //同时并发执行的线程数
+    public static int THREAD_TOTAL = 50;
+    public static int COUNT = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(THREAD_TOTAL);
+        final CountDownLatch countDownLatch = new CountDownLatch(CLIENT_TOTAL);
+        for (int i = 0; i < CLIENT_TOTAL; i++) {
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",COUNT);
+    }
+
+    public static void add(){
+        COUNT++;
+    }
+}
